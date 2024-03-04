cc4c05fb1963d017124d21b91537a4742eb539c3
FilipaMerino <filipaam@outlook.com>
2024-03-04 19:32:29 +0000

    route to single post

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/services", to: "pages#services"
   get "posts", to: "pages#posts"
+  get "posts/:id", to: "posts#show"
 end
