36139fbbe29a4f9e211e8be7846c8dda96f802da
FilipaMerino <filipaam@outlook.com>
2024-03-04 19:37:58 +0000

    fixed routes and files for show all posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get "/about", to: "pages#about"
   get "/services", to: "pages#services"
-  get "posts", to: "pages#posts"
+  get "posts", to: "posts#index"
   get "posts/:id", to: "posts#show"
 end
