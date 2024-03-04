c16940380b7b5fb91aaec5422382418ebd526d86
FilipaMerino <filipaam@outlook.com>
2024-03-04 21:39:25 +0000

    create, update and delete on posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all
   end
@@ -8,6 +7,36 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[post_params])
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params[post_params])
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, status: :see_other
+  end
 
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
